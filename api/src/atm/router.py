3df376c1b69722b73dfbee0859070a8784d3496e
tcaty <ltcatyl@gmail.com>
2023-10-14 18:51:41 +0300

    get endpoints by id add

--- api/src/atm/router.py
+++ api/src/atm/router.py
@@ -2,7 +2,8 @@ import json
 from fastapi import APIRouter
 from contstants import Collection
 from atm.model import Atm
-from typing import List
+from typing import List, Dict
+from fastapi.exceptions import HTTPException
 
 
 router = APIRouter()
@@ -11,7 +12,17 @@ router.tags = [Collection.atms]
 
 @router.get("/all")
 async def get_atms(offset: int = 0, limit: int = 0) -> List[Atm]:
-    with open("atms.json", encoding='utf-8') as json_file:
+    with open("atms.json", encoding="utf-8") as json_file:
         atms = json.load(json_file)[offset : (offset + limit) if limit > 0 else None]
         atms = [Atm(**item) for item in atms]
         return atms
+
+
+@router.get("/{id}")
+async def get_atm_by_id(id: str) -> Atm:
+    with open("atms.json", encoding="utf-8") as json_file:
+        atms: List[Dict] = json.load(json_file)
+        for atm in atms:
+            if atm["id"] == id:
+                return Atm(**atm)
+    raise HTTPException(status_code=404, detail=f"Atm with {id} not found")
--- api/src/office/router.py
+++ api/src/office/router.py
@@ -1,29 +1,50 @@
 import json
 import pandas as pd
-from firestore import firestore
 from fastapi import APIRouter
 from contstants import Collection
 from office.model import Office
-from typing import List
+from typing import List, Dict
+from fastapi.exceptions import HTTPException
 
 
 router = APIRouter()
 router.tags = [Collection.office]
 
+
 def rank(offices: List[dict]) -> List[dict]:
     offices_df = pd.DataFrame(offices)
-    offices_df['workload'] = offices_df['people'] / offices_df['windows']
-    offices_df = offices_df.sort_values(['distance', 'workload'], ascending=[True, True])
-    offices_df.loc[(0 <= offices_df['workload']) & (offices_df['workload'] <= 2), 'workload_type'] = 0
-    offices_df.loc[(2 < offices_df['workload']) & (offices_df['workload'] <= 3.5), 'workload_type'] = 1
-    offices_df.loc[3.5 < offices_df['workload'], 'workload_type'] = 2
-    offices = offices_df.to_dict(orient='records')
+    offices_df["workload"] = offices_df["people"] / offices_df["windows"]
+    offices_df = offices_df.sort_values(
+        ["distance", "workload"], ascending=[True, True]
+    )
+    offices_df.loc[
+        (0 <= offices_df["workload"]) & (offices_df["workload"] <= 2), "workload_type"
+    ] = 0
+    offices_df.loc[
+        (2 < offices_df["workload"]) & (offices_df["workload"] <= 3.5), "workload_type"
+    ] = 1
+    offices_df.loc[3.5 < offices_df["workload"], "workload_type"] = 2
+    offices = offices_df.to_dict(orient="records")
     return offices
+
 
 @router.get("/all")
 async def get_all_offices(offset: int = 0, limit: int = 0) -> List[Office]:
-    with open("offices.json", encoding='utf-8') as json_file:
+    with open("offices.json", encoding="utf-8") as json_file:
         offices = json.load(json_file)
         offices = rank(offices)
-        offices = [Office(**item) for item in offices][offset : (offset + limit) if limit > 0 else None]
+        offices = [Office(**item) for item in offices][
+            offset : (offset + limit) if limit > 0 else None
+        ]
         return offices
+
+
+@router.get("/{id}")
+async def get_office_by_id(id: str) -> Office:
+    with open("offices.json", encoding="utf-8") as json_file:
+        offices: List[Dict] = json.load(json_file)
+        offices = rank(offices)
+        for office in offices:
+            if office["id"] == id:
+                return Office(**office)
+    raise HTTPException(status_code=404, detail=f"Office with {id} not found")
